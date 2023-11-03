51cf3ea30c6e118c61455864518755761469aa14
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-11-03 18:05:10 +0530

    modified the handler and test the game

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
 import asyncio
+import json
+
 import websockets
+
+from connect4 import Connect4, PLAYER1, PLAYER2
 
 
 async def handler(websocket):
-    while True:
-        try:
-            message = await websocket.recv()
-            print(message)
-        except websockets.ConnectionClosedOK:
-            break
+    # Initialize a Connect Four game.
+    game = Connect4()
 
+
+    for player, column, row in [
+        (PLAYER1, 3, 0),
+        (PLAYER2, 3, 1),
+        (PLAYER1, 4, 0),
+        (PLAYER2, 4, 1),
+        (PLAYER1, 2, 0),
+        (PLAYER2, 1, 0),
+        (PLAYER1, 5, 0),
+    ]:
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+        await asyncio.sleep(0.5)
+    event = {
+        "type": "win",
+        "player": PLAYER1,
+    }
+    await websocket.send(json.dumps(event))
 
 
 async def main():
     async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()
+        await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
--- main.js
+++ main.js
@@ -1,6 +1,52 @@
-import {playMove,createBoard} from "./connect4";
+import {playMove,createBoard} from "./connect4.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    const board = doucment.querySelector(".board");
+    const board = document.querySelector(".board");
     createBoard(board);
+
+    const websocket = new WebSocket("ws://localhost:8001/")
+    receiveMoves(board, websocket)
+    sendMoves(board, websocket);
+
+
 });
+
+function sendMoves(board, websocket) {
+  // When clicking a column, send a "play" event for a move in that column.
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+    // Ignore clicks outside a column.
+    if (column === undefined) {
+      return;
+    }
+    const event = {
+      type: "play",
+      column: parseInt(column, 10),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+function showMessage(message){
+    window.setTimeout(() => window.alert(message), 50)
+}
+
+function receiveMoves(board, websocket){
+    websocket.addEventListener("message", ({data}) =>{
+       const event = JSON.parse(data);
+       switch (event.type){
+           case "play":
+               playMove(board, event.player, event.column, event.row);
+               break;
+           case "win":
+               showMessage(`player ${event.player} winds!`);
+               websocket.close(1000);
+               break;
+           case "error":
+               showMessage(event.message);
+               break;
+           default:
+               throw new Error(`Unsupported event type: ${event.type}`);
+       }
+    });
+}
